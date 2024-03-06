97673c72245844ebf82b549866159d0d2a952058
Mohammad Faizan <mdfaiz7411@gmail.com>
2024-03-06 14:18:51 +0530

    updated code

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const userModel = require("./users")
+const userModel = require("./users");
+const passport = require('passport');
+const localStrategy = require('passport-local');
+
+passport.use(new localStrategy(userModel.authenticate()));
 
 router.get('/', function (req, res, next) {
   res.render('index');
@@ -10,6 +14,11 @@ router.get('/register', function (req, res, next) {
   res.render('register');
 });
 
+router.get('/profile', isLoggedIn, function (req, res, next) {
+  res.render('profile');
+});
+
+//password authentication for registration
 router.post('/register', function (req, res, next) {
   const data = new userModel({
     username: req.body.username,
@@ -23,9 +32,29 @@ router.post('/register', function (req, res, next) {
   })
 });
 
-router.get('/profile', function (req, res, next) {
-  res.render('profile');
+//password authentication for login
+router.post('/login', passport.authenticate('local', {
+  failureRedirect: '/',
+  successRedirect: '/profile',
+}), function (req, res, next) {
+
 });
 
+//logout code to logout, this directs to the / route
+router.get('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+})
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/')
+}
 
 module.exports = router;
